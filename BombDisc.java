0583ed056bcca7d76066b7763d03f3099b39320b
daelHacohen <dael12345@gmail.com>
2024-11-18 20:40:02 +0200

    מחיקה

---
+++ BombDisc.java
@@ -0,0 +1,18 @@
+public class BombDisc implements Disc{
+    private Player owner;
+    @Override
+    public Player getOwner() {
+        return owner;
+    }
+
+    @Override
+    public void setOwner(Player player) {
+        this.owner = player;
+
+    }
+
+    @Override
+    public String getType() {
+        return "💣";
+    }
+}
---
+++ UnflippableDisc.java
@@ -0,0 +1,18 @@
+public class UnflippableDisc implements Disc{
+    private Player owner;
+    @Override
+    public Player getOwner() {
+        return owner;
+    }
+
+    @Override
+    public void setOwner(Player player) {
+        this.owner = player;
+
+    }
+
+    @Override
+    public String getType() {
+        return "⭕";
+    }
+}
