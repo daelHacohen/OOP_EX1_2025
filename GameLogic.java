34b50fc8b207ea9875288b420154fa3af84b9838
daelHacohen <dael12345@gmail.com>
2024-11-19 11:21:33 +0200

    מימשתי את הפונ' reset במחלקה GameLogic עכשיו יש את ארבעת הדיסקיות כשמתחילים את המשחק.

--- GameLogic.java
+++ GameLogic.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameLogic implements PlayableLogic{
-    private Disc[][] discsOnBoard = new Disc[8][8];
+    private final Disc[][] discsOnBoard = new Disc[8][8];
    private Player player1;
     private Player player2;
     @Override
@@ -59,6 +59,20 @@ public class GameLogic implements PlayableLogic{
 
     @Override
     public void reset() {
+        for (int i = 0; i < discsOnBoard.length; i++) {
+            for (int j = 0; j < discsOnBoard.length; j++) {
+                discsOnBoard[i][j]=null;
+            }
+        }
+        SimpleDisc d1 =new SimpleDisc(player1);
+        SimpleDisc d2 =new SimpleDisc(player1);
+        SimpleDisc d3 =new SimpleDisc(player2);
+        SimpleDisc d4 =new SimpleDisc(player2);
+        discsOnBoard[3][3]=d1;
+        discsOnBoard[4][4]=d2;
+        discsOnBoard[3][4]=d3;
+        discsOnBoard[4][3]=d4;
+
 
     }
 
